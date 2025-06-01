50882a610c150ea4e44f92216009efbfe8aae828
Elias Müller <elias_benedikt.mueller@porsche.de>
2025-06-01 10:40:44 +0200

    chore: proper pauses in test

--- gatling-test/src/main/kotlin/GatlingScenario.kt
+++ gatling-test/src/main/kotlin/GatlingScenario.kt
@@ -39,7 +39,7 @@ val defaultScenario = scenario("Buy Process").exec { session ->
 }.exec(
     http("createShoppingcartItemMutation").post("/graphql").body(StringBody("#{createShoppingcartItemMutation}"))
         .check(jsonPath("$.data.createShoppingcartItem.id").saveAs("createShoppingcartItemId"))
-).exec { session ->
+).pause(Duration.ofMillis(4000), Duration.ofMillis(7000)).exec { session ->
     session.set(
         "shipmentMethodsQuery",
         "{ \"query\": \"query { shipmentMethods { totalCount nodes { id name baseFees description feesPerItem feesPerKg } } }\" }",
@@ -55,7 +55,7 @@ val defaultScenario = scenario("Buy Process").exec { session ->
 }.exec(
     http("paymentInformationsQuery").post("/graphql").body(StringBody("#{paymentInformationsQuery}"))
         .check(jsonPath("$.data.paymentInformations.nodes[0].id").saveAs("paymentInformationId"))
-).exec { session ->
+).pause(Duration.ofMillis(4000), Duration.ofMillis(7000)).exec { session ->
     val userId = session.getString("userId")
     val addressId = session.getString("addressId")
     val createShoppingcartItemId = session.getString("createShoppingcartItemId")
@@ -65,7 +65,7 @@ val defaultScenario = scenario("Buy Process").exec { session ->
         "createOrderMutation",
         "{ \"query\": \"mutation { createOrder( input: { userId: \\\"$userId\\\" orderItemInputs: { shoppingCartItemId: \\\"$createShoppingcartItemId\\\" couponIds: [] shipmentMethodId: \\\"$shipmentMethodId\\\" } vatNumber: \\\"AB1234\\\" invoiceAddressId: \\\"$addressId\\\" shipmentAddressId: \\\"$addressId\\\" paymentInformationId: \\\"$paymentInformationId\\\" } ) { id paymentInformationId placedAt } }\" }"
     )
-}.exec(
+}.pause(Duration.ofMillis(2000), Duration.ofMillis(5000)).exec(
     http("createOrderMutation").post("/graphql").body(StringBody("#{createOrderMutation}"))
         .check(jsonPath("$.data.createOrder.id").saveAs("createOrderId"))
 ).exec { session ->
